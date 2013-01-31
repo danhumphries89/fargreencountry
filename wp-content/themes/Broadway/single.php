4db004819862e77efedd00d21f47e1b413ab1e7f
Dan Humphries <dan.humphries89@gmail.com>
2013-01-31 10:31:00 +0000

    added meta & source data to single post view

--- wp-content/themes/Broadway/single.php
+++ wp-content/themes/Broadway/single.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
+<?php
+	$meta = get_post_meta( $post->ID );
+
+	//get the source if exists, split to array
+	$source = (isset($meta['source'][0])) ? explode(';', $meta['source'][0]) : "";
+
+	//get the tags for the current post
+	$tags = get_the_tags( $post->ID );
+?>
+
 <div class="single">
 	<header class="header">
 		<h1 class="title"><?php the_title(); ?></h1>
@@ -12,5 +22,20 @@
 		<?php the_content(); ?>
 	</section>
 	<footer class="footer">
+		<div class="meta">
+			<p class="source">
+				<span class="title">Source</span>
+				<a href="<?php echo trim($source[1]); ?>" target="_blank">
+					<span><?php echo $source[0]; ?></span>
+				</a>
+			</p>
+			<p class="tags">
+				<span class="title">Similar Items</span>
+				<?php foreach($tags as $tag) : ?>
+					<a href="<?php echo get_tag_link( $tag->term_id ); ?>">
+						<span><?php echo $tag->name; ?></span>
+					</a>
+				<?php endforeach; ?>
+			</p>
 	</footer>
 </div>
