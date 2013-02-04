46c0c4741b431e9970d74f7735c9d2b955879bb4
Dan Humphries <dan.humphries89@gmail.com>
2013-02-04 21:31:43 +0000

    removed mainmenu from features section for time being - may decide to add back in

--- wp-content/themes/Broadway/assets/js/scripts.js
+++ wp-content/themes/Broadway/assets/js/scripts.js
@@ -3,13 +3,15 @@ console.log(json_posts);
 /** Scrolling Functions **/
 $(window).scroll(function(){
 
-	var mainmenu = $('.single > .main_header');
-	var scrollPosition = $(window).scrollTop();
+	//var mainmenu = $('.single > .main_header');
+	//var scrollPosition = $(window).scrollTop();
 
-	var sectionHeight = ($('section.content').height() - 75);
+	//var sectionHeight = ($('section.content').height() - 50);
 
 	//show and hide the header
-	if(scrollPosition >= sectionHeight){ $(mainmenu).addClass("visible_header"); $(mainmenu).fadeIn('fast'); }
-	if(scrollPosition <= sectionHeight){ $(mainmenu).addClass("visible_header"); $(mainmenu).fadeOut('fast'); }
+	/**--ENABLE IF USED--**/
+	//if(scrollPosition >= sectionHeight){ $(mainmenu).addClass("visible_header"); $(mainmenu).fadeIn('fast'); }
+	//if(scrollPosition <= sectionHeight){ $(mainmenu).addClass("visible_header"); $(mainmenu).fadeOut('fast'); }
+	/** --------------- **/
 
 });
